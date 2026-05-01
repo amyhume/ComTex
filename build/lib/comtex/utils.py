601462c5ade4c3b4e227e3025d4edb891aa78906
amyhume <amyhume66@gmail.com>
2026-05-01 09:54:24 +0100

    adding package set up steps

---
+++ build/lib/comtex/utils.py
@@ -0,0 +1,2 @@
+def package_is_working():
+    print('YAY')
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='ComTex',
+   version='0.1',
+   packages=find_packages(),
+   author='Amy Hume',
+   author_email='amyhume66@gmail.com',
+   description='Contains functions and scripts for media complexity processing',
+   url='https://github.com/amyhume/ComTex',
+   python_requires='>=3.6',
+)
